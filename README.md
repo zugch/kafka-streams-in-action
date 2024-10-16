910ea9bf55a58001a3dfb20da1a4c66b3f74fcd3
Thomas Müller <thomas.mueller@esentri.com>
2024-10-16 13:59:12 +0200

    refacoring of bestellungen importer

--- README.md
+++ README.md
@@ -18,11 +18,9 @@
 ### Run
 
 1. Im Ordner `/scripts/` `./start-services.sh` ausführen um Kafka, AKHQ, Prometheus und Grafana zu starten.
+2. Anlegen der Topics mittels Kafka Console und der beigefügten `./create_topiucs.sh` im Ordner `/scripts/`:
 2. In den verzeichnissen `bestellungen-simulator-app`, `monitoring-app` und `kafka-streaming-app` jeweils die Appliktion
    mit java -jar im jeweilgen `target`-Verzeichnis starten.
-3. 10 Kunden anlegen mit `curl http://localhost:8080/init-kunden/10`
-4. 50 Artikel anlegen mit `curl http://localhost:8080/init-artikel/50`
-5. 500 Bestellungen anlegen mit `curl http://localhost:8080/bestellungen/500`
 
 ---
 
--- bestellungen-simulator-app/src/main/java/de/thmWeb/kafka/kafka/streaming/bestellungenSimulator/initData/BestellungenScheduler.java
+++ bestellungen-simulator-app/src/main/java/de/thmWeb/kafka/kafka/streaming/bestellungenSimulator/initData/BestellungenScheduler.java
@@ -5,19 +5,37 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+
 @Component
 @Slf4j
 public class BestellungenScheduler {
 
     private final BestellungSimulator bestellungSimulator;
+    private final ArtikelImporter artikelImporter;
+    private final KundenImporter kundenImporter;
+    private boolean isInitialized = false;
 
-    public BestellungenScheduler(BestellungSimulator bestellungSimulator) {
+
+    public BestellungenScheduler(BestellungSimulator bestellungSimulator, ArtikelImporter artikelImporter, KundenImporter kundenImporter) {
         this.bestellungSimulator = bestellungSimulator;
+        this.artikelImporter = artikelImporter;
+        this.kundenImporter = kundenImporter;
     }
 
 
-    @Scheduled(initialDelay = 20000, fixedRate = 1000)
+    @Scheduled(initialDelay = 10000, fixedRate = 1000)
     public void simulateBestellungen() {
+        if (!isInitialized) {
+            try {
+                log.info("import Artikel and Kunden");
+                artikelImporter.importArtikel(50);
+                kundenImporter.importKunden(10);
+                isInitialized = true;
+            } catch (IOException e) {
+                log.error(e.getMessage());
+            }
+        }
         bestellungSimulator.simulateBestellungen(1);
     }
 }
